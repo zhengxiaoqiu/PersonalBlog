c046dc87a20565259a78ddcd94ddc3892e14b6ec
zhengxiaoqiu <42570346+zhengxiaoqiu@users.noreply.github.com>
2020-04-27 10:31:59 +0800

    Update DBUtil.js

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -5,7 +5,7 @@ function createConnection () {
         host: '127.0.0.1',
         port: '3306',
         user: 'root',
-        password: 'zxq19960605',
+        password: '111111',
         database: 'my_blog'
     });
 
